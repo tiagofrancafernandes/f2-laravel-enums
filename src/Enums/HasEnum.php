504f47522c55005c2195bf72e6952d9163287e28
Tiago França <tiago@tiagofranca.com>
2023-01-31 18:44:52 -0300

    new methods enum and enumList for HasEnum trait
    On branch master
    Changes to be committed:
        modified:   src/Enums/HasEnum.php

--- src/Enums/HasEnum.php
+++ src/Enums/HasEnum.php
@@ -2,16 +2,69 @@
 
 namespace TiagoF2\Enums;
 
+use TiagoF2\Enums\EnumInterface;
+use TiagoF2\Enums\Enum;
+
 trait HasEnum
 {
-    public static function enums()
+    /**
+     * enum function
+     *
+     * @param boolean $safeMode
+     *
+     * @return Enum|null
+     */
+    public static function enum(bool $safeMode = false)
     {
+        $classToGetEnum = static::class;
         $instance = new static();
-        $enumClassName =
-            property_exists(static, 'enumClass') &&
+
+        $enumClass =
+            property_exists($instance, 'enumClass') &&
             $instance->enumClass &&
             \is_string($instance->enumClass)
             ? $instance->enumClass
-            : \null;
+            : "{$classToGetEnum}Enum";
+
+        if (!class_exists($enumClass)) {
+            if ($safeMode) {
+                return \null;
+            }
+
+            throw new \Exception(
+                "{$enumClass} do not exists. "
+                    . "Create {$enumClass} or add 'enumClass' property on {$classToGetEnum} class."
+            );
+        }
+
+        $implementsInterface = in_array(EnumInterface::class, class_implements($enumClass), true);
+
+        if (!$implementsInterface) {
+            if ($safeMode) {
+                return \null;
+            }
+
+            throw new \Exception("{$enumClass} do not implements " . EnumInterface::class);
+        }
+
+        return new $enumClass();
+    }
+
+    /**
+     * enumList function
+     *
+     * @param bool $onlyIds
+     * @param boolean $safeMode
+     *
+     * @return array|null
+     */
+    public static function enumList(bool $onlyIds = false, bool $safeMode = false): array
+    {
+        /**
+         * @var Enum $enum
+         */
+        $enum = static::enum($safeMode);
+
+        return (array) ($enum ? $enum->enumList($onlyIds) : []);
     }
 }
